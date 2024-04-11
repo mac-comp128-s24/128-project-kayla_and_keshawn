7bb40d6d76734e8d19f3fb4c050b031dc675f8ed
kboughto <kroach3833@gmail.com>
2024-04-11 18:47:47 -0500

    made multiple classes (walk, elephant and main class)

---
+++ src/MultiDimensionalCourse/DimensionalCourse.java
@@ -0,0 +1,9 @@
+package MultiDimensionalCourse;
+
+import edu.macalester.graphics.CanvasWindow;
+
+public class DimensionalCourse {
+    
+    private CanvasWindow canvas;
+    private ElephantDude elephant;
+}

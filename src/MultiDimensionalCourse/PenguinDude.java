af415bf753c4813fcb1145fbdc3535a38b436f58
kboughto <kroach3833@gmail.com>
2024-04-15 15:24:03 -0500

    replaced penguin multiple times

--- src/MultiDimensionalCourse/PenguinDude.java
+++ src/MultiDimensionalCourse/PenguinDude.java
@@ -7,7 +7,7 @@ import edu.macalester.graphics.Image;
 public class PenguinDude extends GraphicsGroup{
     private Image penguin;
     public PenguinDude(CanvasWindow canvas) {
-        penguin =  new Image(0,canvas.getHeight() * 0.75, "penguin.jpeg");
+        penguin =  new Image(0,canvas.getHeight() * 0.75, "otherpenguin.jpg");
         penguin.setMaxHeight(canvas.getHeight() * 0.15);
         penguin.setMaxWidth(canvas.getHeight() * 0.15);
         this.add(penguin);

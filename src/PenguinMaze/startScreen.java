b7cede7a7e7046da00273d553d4651d7c1469dd9
Kayla Olivas <132276482+kolivas380@users.noreply.github.com>
2024-04-26 10:21:42 -0500

    Added in more levels

--- src/PenguinMaze/startScreen.java
+++ src/PenguinMaze/startScreen.java
@@ -1,7 +1,12 @@
 package PenguinMaze;
 
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.FontStyle;
+import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.GraphicsText;
 import edu.macalester.graphics.events.Key;
+import edu.macalester.graphics.ui.Button;
+import edu.macalester.graphics.ui.TextField;
 
 public class startScreen {
     private CanvasWindow canvas;
@@ -11,5 +16,19 @@ public class startScreen {
     private int lives;
     public startScreen(){
         canvas = new CanvasWindow("Penguin Maze", 600, 300);
+        
+    }
+    private void setupUI(){
+        // mazeDisplay = new GraphicsText();
+        // mazeDisplay.setFont("monospaced, Courier New", FontStyle.PLAIN, 14);
+        // canvas.add(mazeDisplay, 50, 50);
+
+        GraphicsGroup uiGroup = new GraphicsGroup();
+
+        TextField filenameField = new TextField();
+        filenameField.setText("maze-2");
+
+        Button loadButton = new Button("Load Maze");
+        loadButton.onClick( () -> maze.loadMaze("MazePattern1.txt") );
     }
 }
